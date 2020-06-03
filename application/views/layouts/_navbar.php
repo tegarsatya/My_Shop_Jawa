dc0db22cd63469b768deabcc972b2b16e5c0f95a
Backend Server <backendserver@gmail.com>
2020-06-03 22:02:03 +0700

    Crud Product and User(Pengguna)

--- application/views/layouts/_navbar.php
+++ application/views/layouts/_navbar.php
@@ -12,10 +12,10 @@
 				<li class="nav-item dropdown">
 					<a href="#" class="nav-link dropdown-toggle" id="dropdown-1" data-toggle="dropdown" aria-hospopup="true" aria-expanded="false">Maneg</a>
 					<div class="dropdown-menu" aria-labelledby="dropdown-1">
-						<a href="<?= base_url('/category')?>" class="dropdown-item"> Kategori</a>
-						<a href="admin-product.html" class="dropdown-item">Produk</a>
+						<a href="<?= base_url('category')?>" class="dropdown-item"> Kategori</a>
+						<a href="<?= base_url('product')?>" class="dropdown-item">Produk</a>
 						<a href="admin-order.html" class="dropdown-item"> Order</a>
-						<a href="admin-users.html" class="dropdown-item">Pengguna</a>
+						<a href="<?= base_url('user')?>" class="dropdown-item">Pengguna</a>
 					</div>
 				</li>
 			</ul>
